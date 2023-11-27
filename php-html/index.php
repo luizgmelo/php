2fc6a6077cbd31c0bd20f7c1c3faa3a75d9f5670
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-11-27 12:31:32 -0300

    HTML with PHP

---
+++ php-html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Apprentice - HTML</title>
+</head>
+<body>
+    <h1><?= strtoupper('I can write HTML');?></h1>
+</body>
+</html>
